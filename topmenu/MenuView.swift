d4782bcd3bf68403538c90bfd9e2b80383575e74
Justas T <j.trimailovas@gmail.com>
2015-10-12 14:32:54 +0300

    Draw cpu usage in menu bar

--- topmenu/MenuView.swift
+++ topmenu/MenuView.swift
@@ -13,6 +13,7 @@ import SystemKit
 class MenuView: NSObject {
   let button: NSStatusBarButton
   var sys = System()
+  var history_queue = StatsHistoryQueue<Double>()
 
   init(statusItem: NSStatusItem) {
     self.button = statusItem.button!
@@ -31,28 +32,28 @@ class MenuView: NSObject {
     print(cpuUsage)
 
     let comboUsage = cpuUsage.system + cpuUsage.user
-    self.button.title = String(format: "%.2f %%", comboUsage)
+//    self.button.title = String(format: "%.2f %%", comboUsage)
+    history_queue.enqueue(comboUsage)
+    self.drawMenuImage()
   }
 
   func drawMenuImage() {
     let path: NSBezierPath = NSBezierPath()
-    path.moveToPoint(NSPoint(x: 0, y: 15))
-    path.lineToPoint(NSPoint(x: 10, y: 15))
-    path.lineToPoint(NSPoint(x: 10, y: 25))
-    path.lineToPoint(NSPoint(x: 20, y: 25))
-    path.lineToPoint(NSPoint(x: 20, y: 35))
-    path.lineToPoint(NSPoint(x: 30, y: 35))
-    path.lineToPoint(NSPoint(x: 30, y: 0))
 
-    let fillColor = NSColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 1.0)
-    fillColor.set()
-    path.fill()
-    path.lineWidth = 5
-    path.stroke()
+    path.moveToPoint(NSPoint(x: history_queue.length - history_queue.items.count + 1, y: 0))
 
+    for (index, value) in history_queue.items.enumerate() {
+      let x_coordinate = history_queue.length - history_queue.items.count + 1 + index
+      let normalized_value: Int = Int(value / 5)
+      print(x_coordinate, normalized_value)
+      path.lineToPoint(NSPoint(x: x_coordinate, y: normalized_value))
+    }
+    path.lineToPoint(NSPoint(x: 20, y: 0))
     path.closePath()
 
-    let img: NSImage = NSImage(size: NSSize(width: 50, height: 50))
+    let img: NSImage = NSImage(size: NSSize(width: 20, height: 20))
+    // set as template, so switching to/from dark mode would work
+    img.template = true
     img.lockFocus()
     path.fill()
     img.unlockFocus()
